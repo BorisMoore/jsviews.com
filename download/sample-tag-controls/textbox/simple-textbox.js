43a6c271d7ab196a8076e55e61e40a3d8806ab6b
Boris Moore <borismoore@gmail.com>
2013-09-26 20:52:08 -0700

    Added additional files to previous commit

---
+++ download/sample-tag-controls/textbox/simple-textbox.js
@@ -0,0 +1,21 @@
+﻿/*
+ * Sample JsViews tag control: Minimalist {{textbox}} control
+ * http://www.jsviews.com/download/sample-tag-controls/textbox/simple-textbox.js
+ * Used in samples: http://www.jsviews.com/#samples/tag-controls/edit/textbox
+ * Copyright 2013, Boris Moore
+ * Released under the MIT License.
+ */
+
+(function($) {
+"use strict";
+
+$.views.tags({
+  textbox: {
+    onAfterLink: function() {
+      this.linkedElem = this.contents("input");
+    },
+    template: "<input/>"
+  }
+});
+
+})(this.jQuery);
