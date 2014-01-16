8353e1b2fbef30920533c19a815a1a0a9e7ac0b5
BorisMoore <borismoore@gmail.com>
2014-01-15 17:21:51 -0800

    rename jsonview.js to revert casing to lower case

---
+++ download/sample-tag-controls/jsonview/jsonview2.js
@@ -0,0 +1,54 @@
+﻿/*
+ * Sample JsViews tag control: {{jsonview}} control
+ * http://www.jsviews.com/download/sample-tag-controls/jsonview/jsonview.js
+
+ * Copyright 2013, Boris Moore
+ * Released under the MIT License.
+*/
+
+(function($) {
+  "use strict";
+
+  function isObject(val) {
+    return val && typeof val === "object";
+  }
+
+  function notEmpty(val) {
+    return $.views.tags.props.getTgt(val).length;
+  }
+
+  notEmpty.depends = "*";
+
+  $.views.tags("jsonview", {
+    template: {
+      markup:
+        '{{if ~isArray(#data)}}'
+        + '[{^{if length}}'
+        + '<ul class="jsonview">'
+          + '{^{for #data}}'
+          + '<li>{^{jsonview/}}{^{if #index < #parent.data.length-1}},{{/if}}</li>'
+          + '{{/for}}'
+        + '</ul>'
+        + '{{/if}}]'
+      + '{{else ~isObject(#data)}}'
+        + '{{^{if ~notEmpty(#data)}}'
+        + '<ul class="jsonview">'
+          + '{^{props #data}}'
+          + '<li>'
+            + '<b>{^{>key}}: </b>'
+            + '{^{jsonview prop/}}{^{if #index < #parent.data.length-1}},{{/if}}'
+          + '</li>'
+          + '{{/props}}'
+        + '</ul>'
+        + '{{/if}}}'
+      + '{{else}}'
+        + '"{^{:#data}}"'
+      + '{{/if}}',
+      helpers: {
+        isObject: isObject,
+        notEmpty: notEmpty,
+        isArray: $.isArray
+      }
+    }
+  });
+})(this.jQuery);
